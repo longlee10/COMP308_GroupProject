f8bac45f9b762304014b79b110ffa078e5415ffd
Lanh Hoang <lanhhoang@users.noreply.github.com>
2024-03-25 11:18:29 -0400

    Setup API gateway using Apollo

---
+++ server/server.js
@@ -0,0 +1,33 @@
+import { ApolloServer } from "apollo-server-express";
+import { ApolloGateway } from "@apollo/gateway";
+
+import configureExpress from "./config/express.js";
+
+// Create a new Express application instance
+const app = configureExpress();
+
+// Define the port
+const port = process.env.API_GATEWAY_PORT || 4000;
+
+// Configure the Apollo Gateway
+const gateway = new ApolloGateway({
+  serviceList: [],
+});
+
+// Initialize an Apollo Server with Apollo Gateway
+const server = new ApolloServer({
+  gateway,
+  subscriptions: false,
+});
+
+// Apply the Apollo Server to the Express application
+server.start().then(() => {
+  server.applyMiddleware({ app });
+
+  // Start the Express server
+  app.listen({ port }, () => {
+    console.log(
+      `🚀 Gateway ready at http://localhost:${port}${server.graphqlPath}`
+    );
+  });
+});
