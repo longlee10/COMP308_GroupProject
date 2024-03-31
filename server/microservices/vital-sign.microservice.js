fe8c0f0c10e5064677fe716cafb3055d20639be4
Lanh Hoang <lanhhoang@users.noreply.github.com>
2024-03-31 15:38:17 -0400

    Refactor Vital Sign microservice

--- server/microservices/vital-sign.microservice.js
+++ server/microservices/vital-sign.microservice.js
@@ -1,23 +1,21 @@
+import dotenv from "dotenv";
 dotenv.config();
 
 // Set the 'NODE_ENV' variable
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 import cors from "cors";
-import dotenv from "dotenv";
-import jwt from "jsonwebtoken";
 import { ApolloServer } from "apollo-server-express";
 import { buildFederatedSchema } from "@apollo/federation";
 
 import config from "../config/config.js";
 import configureExpress from "../config/express.js";
 import configureMongoose from "../config/mongoose.js";
+import auth from "../middlewares/auth.middleware.js";
 
 // Import the GraphQL schema
 import typeDefs from "../schemas/vital-sign.schema.js";
 import resolvers from "../resolvers/vital-sign.resolver.js";
-
-const jwtSecretKey = config.secretKey;
 
 // Create a new Mongoose connection instance
 const db = configureMongoose(config.vitalSignDb);
@@ -46,27 +44,7 @@ app.use(
 const server = new ApolloServer({
   // Use buildFederatedSchema to combine your schema and resolvers
   schema: buildFederatedSchema([{ typeDefs, resolvers }]),
-  context: ({ req, res }) => {
-    const token = req.cookies["token"];
-
-    console.log("in vital sign microservice context - token: ", token);
-
-    if (!token) {
-      req.isAuthenticated = false;
-      return { req, res };
-    }
-
-    try {
-      const decoded = jwt.verify(token, jwtSecretKey);
-      console.log("in vital sign microservice context - decoded: ", decoded);
-      const { username } = decoded;
-      req.isAuthenticated = username ? true : false;
-      return { req, res };
-    } catch (error) {
-      console.error("Error in vital sign microservice context: ", error);
-      throw new Error("Your session expired. Sign in again.");
-    }
-  },
+  context: auth,
 });
 
 server.start().then(() => {
