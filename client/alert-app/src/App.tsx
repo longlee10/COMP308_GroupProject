a1ee261423f924087b85da69c7cfb8565c9cd59c
longquan98@gmail.com <longquan98@gmail.com>
2024-04-03 17:47:14 -0400

    Set up Apollo Client in all micro frontend

--- client/alert-app/src/App.tsx
+++ client/alert-app/src/App.tsx
@@ -1,10 +1,25 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4003/graphql", // Set this to your actual GraphQL endpoint
+  cache: new InMemoryCache(),
+  credentials: "include",
+});
 
 function App() {
   return (
-    <>
-      <p>Hello Alert</p>
-    </>
+    <Router>
+      <div className="App">
+        <ApolloProvider client={client}>
+          <Routes>
+            <Route path="/alert" />
+          </Routes>
+        </ApolloProvider>
+      </div>
+    </Router>
   );
 }
 
--- client/motivation-tip-app/src/App.tsx
+++ client/motivation-tip-app/src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4005/graphql", // Set this to your actual GraphQL endpoint
+  cache: new InMemoryCache(),
+  credentials: "include",
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="App">
+        <ApolloProvider client={client}>
+          <Routes>
+            <Route path="/motivation" />
+          </Routes>
+        </ApolloProvider>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
