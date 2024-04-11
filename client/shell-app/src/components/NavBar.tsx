3fd63943fc31337a21bce66f378111c65033e4d8
longquan98@gmail.com <longquan98@gmail.com>
2024-04-11 19:08:09 -0400

    Rebuild NavBar with Shadcn

--- client/shell-app/src/components/NavBar.tsx
+++ client/shell-app/src/components/NavBar.tsx
@@ -1,32 +1,58 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
 import { Link } from "react-router-dom";
 
 function NavBar() {
+  const navBars = [
+    { name: "Home", path: "/" },
+    { name: "Vital Signs", path: "/vital-sign" },
+    { name: "Alerts", path: "/alert" },
+    { name: "Motivation Tips", path: "/motivation" },
+    { name: "Symptoms", path: "/symptom" },
+    { name: "Game", path: "/game" },
+  ];
   return (
-    <Navbar expand="lg" className="bg-body-tertiary mb-5">
-      <Container>
-        <Navbar.Brand>Vital Hub</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto gap-3">
-            <Nav.Item>
-              <Link to="/vital-sign">Vital Sign</Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Link to="/alert">Alert</Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Link to="/motivation">Motivation</Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Link to="/symptom">Symptom</Link>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className="mb-5 p-3">
+      <NavigationMenu>
+        <NavigationMenuList>
+          {navBars.map((navBar, index) => {
+            return (
+              <NavigationMenuItem key={index}>
+                <Link to={navBar.path} className={navigationMenuTriggerStyle()}>
+                  {navBar.name}
+                </Link>
+              </NavigationMenuItem>
+            );
+          })}
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+    // <Navbar expand="lg" className="bg-body-tertiary mb-5">
+    //   <Container>
+    //     <Navbar.Brand>Vital Hub</Navbar.Brand>
+    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    //     <Navbar.Collapse id="basic-navbar-nav">
+    //       <Nav className="me-auto gap-3">
+    //         <Nav.Item>
+    //           <Link to="/vital-sign">Vital Sign</Link>
+    //         </Nav.Item>
+    //         <Nav.Item>
+    //           <Link to="/alert">Alert</Link>
+    //         </Nav.Item>
+    //         <Nav.Item>
+    //           <Link to="/motivation">Motivation</Link>
+    //         </Nav.Item>
+    //         <Nav.Item>
+    //           <Link to="/symptom">Symptom</Link>
+    //         </Nav.Item>
+    //       </Nav>
+    //     </Navbar.Collapse>
+    //   </Container>
+    // </Navbar>
   );
 }
 
